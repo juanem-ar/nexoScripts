7028123c87ad35833d584ee2cf1a4b37541824e1
Juan Manuel <juanemlopez@gmail.com>
2023-04-19 17:24:33 -0300

    Update test2.py
    Se agregan comentarios

--- test2.py
+++ test2.py
@@ -6,12 +6,17 @@ driver = webdriver.Chrome();
 
 # Navegar a posadas.lastipas.com.ar
 driver.get("https://posadas.lastipas.com.ar");
+
+# Se espera a que cargue la pagina 5s
 driver.implicitly_wait(5)
 
+# Se selecciona la lista contenedora de peliculas
 movies_list = driver.find_elements(By.ID, 'listaCartelera');
 movie = driver.find_elements(By.CLASS_NAME, 'col-sm-6');
 
+# Se itera la lista de elementos para imprimir cada uno de ellos
 for movie in movies_list:
     print(movie.text);
 
+# Cerrar la ventana
 driver.quit();
